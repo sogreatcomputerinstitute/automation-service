930e374b0516c9e1a50b66a12416e586f2f01e67
Ask Ninja <destiny20202010@gmail.com>
2026-08-03 15:42:56 +0100

    Update Trustaautomate.js

--- Trustaautomate.js
+++ Trustaautomate.js
@@ -120,7 +120,7 @@ async function runSingleUserTask(userCredentials, index) {
         console.log('Navigating to sponsored tasks...');
         await page.goto('https://trusta.live/sponsored', { waitUntil: 'domcontentloaded', timeout: 30000 });
         await new Promise(resolve => setTimeout(resolve, 10000));
-        const sponsoredTaskButton = 'button[data-slot="button"]';
+        const sponsoredTaskButton = 'button[data-slot="button"] group/button inline-flex items-center justify-center border border-transparent bg-clip-padding whitespace-nowrap outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [a]:hover:bg-primary/80 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 flex-1 h-10 rounded-full bg-primary-gradient text-white text-[13px] font-black shadow-lg shadow-blue-500/20 transition-all active:scale-95';
         try {
             await page.waitForSelector(sponsoredTaskButton, { timeout: 10000 });
             await page.click(sponsoredTaskButton);
